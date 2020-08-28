8c13482c3d8d0eb025da681b3b559172774c6113
nianb <nianb@actec.com.cn>
2020-08-28 13:22:39 +0800

    rename

---
+++ Pods/Target Support Files/Pods-AcTECBLE/Pods-AcTECBLE-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AcTECBLE : NSObject
+@end
+@implementation PodsDummy_Pods_AcTECBLE
+@end
--- Pods/Target Support Files/Pods-CSRmeshDemo/Pods-CSRmeshDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CSRmeshDemo : NSObject
-@end
-@implementation PodsDummy_Pods_CSRmeshDemo
-@end
